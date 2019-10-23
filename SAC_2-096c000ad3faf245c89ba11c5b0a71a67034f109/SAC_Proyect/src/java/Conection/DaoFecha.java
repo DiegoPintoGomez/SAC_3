4a5650b48e474d1a3253876382ee6ffd5c2e9652
DiegoPintoGomez <44154154+DiegoPintoGomez@users.noreply.github.com>
2019-10-22 23:17:54 -0600

    Dao

--- SAC_2-096c000ad3faf245c89ba11c5b0a71a67034f109/SAC_Proyect/src/java/Conection/DaoFecha.java
+++ SAC_2-096c000ad3faf245c89ba11c5b0a71a67034f109/SAC_Proyect/src/java/Conection/DaoFecha.java
@@ -7,11 +7,18 @@ package Conection;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.GregorianCalendar;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -23,17 +30,17 @@ public class DaoFecha {
         return DBConection.mariaDBconn();
     }
 
-  public static void  inserFecha(String contacto,Calendar date) throws SQLException{
+  public static void  inserFecha(Calendar date, String contacto) throws SQLException{
         String SQL ="INSERT INTO fecha (fecha,contacto) "
                 + "VALUES(?,?)";
         
         try(Connection conn = connect();
                PreparedStatement pstmt = conn.prepareStatement(SQL,
                 Statement.RETURN_GENERATED_KEYS)){
-            SimpleDateFormat format = new SimpleDateFormat("yyy-MM-dd");
+            SimpleDateFormat format = new SimpleDateFormat("yyyyMMdd");
             String dateString =format.format(date.getTime());
-            pstmt.setString(1, contacto);
-            pstmt.setString(2,dateString);
+            pstmt.setString(1, dateString);
+            pstmt.setString(2,contacto);
             pstmt.executeUpdate();
         }
          catch (SQLException ex) {
@@ -41,6 +48,36 @@ public class DaoFecha {
         }
       }
 
+     public static List<Calendar> getAllFecha_Contacto(String Cont){
+        ArrayList<Calendar> cal = new ArrayList<Calendar>();
+        
+         String sql = "select contacto,fecha "
+                 + "from fecha where contacto = ?";
+       
+         try (Connection conn =connect();
+                PreparedStatement pstmt = conn.prepareStatement(sql)){
+                pstmt.setString(1, Cont);
+                ResultSet rs = pstmt.executeQuery();
+             while(rs.next()){
+                    try {
+                        cal.add(getFecha(rs));
+                    } catch (ParseException ex) {
+                        Logger.getLogger(DaoFecha.class.getName()).log(Level.SEVERE, null, ex);
+                    }
+             }
+         } catch (SQLException ex) {
+            Logger.getLogger(DaoOperadora.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return cal;
+     
+    }   
 
-    
+    private static Calendar getFecha(ResultSet rs) throws SQLException, ParseException {
+      String date = rs.getString("fecha");
+      Date da = new SimpleDateFormat("yyyyMMDD").parse(date);
+      Calendar Greg = new GregorianCalendar();
+      Greg.setTime(da);
+      return Greg;
+    }
+  
 }
